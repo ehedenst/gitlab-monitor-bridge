0aa52fcf5231338d72d66c394153f16af3d5ba37
Erik Hedenström <erik@hedenstroem.com>
2018-10-15 00:21:43 +0200

    First release

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # gitlab-monitor-bridge
 
 HTTP to MQTT bridge that uses gitlab system hooks to wake up gitlab monitor
+
+## Deploying
+
+gcloud functions deploy relayToMQTT --runtime nodejs6 --trigger-http
+
+## Emulator
+
+npm install -g @google-cloud/functions-emulator
+functions deploy relayToMQTT --trigger-http
---
+++ index.js
@@ -0,0 +1,26 @@
+var mqtt = require('mqtt');
+
+var token = process.env.GITLAB_TOKEN || '';
+
+// TODO: parse these from query params
+var mqtt_url = process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883';
+var topic = process.env.CLOUDMQTT_TOPIC || 'gitlab';
+
+exports.relayToMQTT = (req, res) => {
+  if (req.get('X-Gitlab-Token') == token) {
+    var client = mqtt.connect(mqtt_url);
+    client.on('error', function (err) {
+      res.status(503).send(err.message);
+      client.end(true);
+    });
+    client.on('connect', function () {
+       client.publish(topic, JSON.stringify(req.body), function () {
+        client.end(function () {
+          res.status(204).end();
+        });
+      });
+    });
+  } else {
+    res.status(403).send('Invalid token');
+  }
+};
